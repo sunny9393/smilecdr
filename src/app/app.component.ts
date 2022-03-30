2933220011653f38f952e535d45e2206f36eef3b
Sunny Parikh <SParikh@arbormemorial.com>
2022-03-30 11:43:31 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as data from 'src/assets/questionnaire.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'smile-cdr';
+  title: any;
+  myForm: FormGroup;
+  displayData: any;
+
+  storeJsonData: any[];
+  myNewForm: FormGroup;
+  displayNewData: any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      allergies: ["False"],
+      gender: ["Male"],
+      date: [""],
+      country: [""],
+      status: [""],
+      smoke: [""],
+      drink: [""]
+    });
+
+    this.createNewForm();
+  }
+
+  onSubmit() {
+    this.displayData = JSON.stringify(this.myForm.value);
+  }
+
+  onSubmitNewForm() {
+    this.displayNewData = JSON.stringify(this.myNewForm.value);
+  }
+
+  createNewForm() {
+    this.storeJsonData = data.item;
+
+    this.myNewForm = this.fb.group({
+      newAllergies: ["False"],
+      newGender: ["Male"],
+      newDate: [""],
+      newCountry: [""],
+      newStatus: [""],
+      newSmoke: [""],
+      newDrink: [""]
+    })
+  }
+
 }
